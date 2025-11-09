4506488dbf17d653966e6add83de6e858854bb9f
mahmudulislam299 <mahmudulislam299@gmail.com>
2025-11-09 19:39:43 +0600

    adf

--- Flower_Shop/src/Components/Navbar/Navbar.jsx
+++ Flower_Shop/src/Components/Navbar/Navbar.jsx
@@ -4,17 +4,25 @@ import { Profile } from "./Profile";
 
 export const Navbar = () => {
   return (
-    <div className="navbar">
-      <div>
-        <Link to="/">
+    <header className="navbar">
+      {/* Brand section */}
+      <div className="navbar_brand">
+        <Link to="/" className="navbar_brand_link">
           <img
             className="logo_flower_shop"
-            src="cake-logo.jpg" 
-            alt="Logo"
+            src="cake-logo.jpg"
+            alt="Shahbag Cake Shop Logo"
           />
+          <div className="navbar_brand_text">
+            <span className="navbar_brand_title">Shahbag Cake Shop</span>
+            <span className="navbar_brand_tagline">
+              Fresh cakes, every celebration 🎂
+            </span>
+          </div>
         </Link>
       </div>
 
+      {/* Search bar */}
       <div className="navbar_input">
         <input
           className="input_nav"
@@ -24,9 +32,10 @@ export const Navbar = () => {
         <i className="fa fa-search"></i>
       </div>
 
+      {/* Profile / Auth */}
       <div className="navbar_login">
         <Profile />
       </div>
-    </div>
+    </header>
   );
 };
